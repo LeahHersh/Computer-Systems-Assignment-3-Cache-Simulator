15addddff8a82d702287bd4e4e3f43180d07a693
LeahHersh <lhersh411@gmail.com>
2024-03-09 18:02:51 -0500

    Setup

--- csim.h
+++ csim.h
@@ -7,12 +7,14 @@
 
 struct Slot {
   uint32_t tag;
-  bool valid;
-  uint32_t load_ts, access_ts;
+  bool valid = false; 
+  uint32_t load_ts;  // Timestamp in simulation time/count of loads and stores
+  uint32_t access_ts;
 };
 
 struct Set {
   std::vector<Slot> slots;
+  // might need to add a map later, std: :map<uint32_t, Slot *> index;
 };
 
 struct Cache {
--- main.cpp
+++ main.cpp
@@ -1,7 +1,32 @@
+#ifndef MAIN_CPP
+#define MAIN_CPP
 #include "csim.h"
-
+#include "csim.cpp"
+#include <stdio.h>
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <string>
 
 int main(int argc, char *argv[])
 {
+    Cache cache;
+    
+    cache.sets.resize(int(argv[0]));
+    int blocks_per_set = int(argv[1]);
+    int block_size = int(argv[2]);
+    bool write_allocate = argv[3]== "write-allocate" ? true : false;  // Okay?
+    bool write_back = argv[4]== "write-back" ? true : false;
+    std::string eviction_policy = argv[5];
+
+    if(block_size < 4 || ceil(block_size) != floor(block_size) || ceil(blocks_per_set) != floor(blocks_per_set) ||
+      (write_back && !write_allocate)) {
+
+        std::cerr << "Invalid configuration\n";
+        return 1;
+    }
+
 
 }
+
+#endif
