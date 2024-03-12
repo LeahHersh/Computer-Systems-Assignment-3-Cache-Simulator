25ea5107f9d8119bc7598f5cba3cffb50378351a
LeahHersh <lhersh411@gmail.com>
2024-03-12 00:16:03 -0400

    Completed setup

--- csim.h
+++ csim.h
@@ -10,13 +10,20 @@ struct Slot {
   bool valid = false; 
   uint32_t load_ts;  // Timestamp in simulation time/count of loads and stores
   uint32_t access_ts;
+
+  int update_load_ts(int new_load_ts) { load_ts = new_load_ts; }
+
+  int update_access_ts(int new_access_ts) { access_ts = new_access_ts; }
 };
 
 struct Set {
   std::vector<Slot> slots;
-  // might need to add a map later, std: :map<uint32_t, Slot *> index;
+  // might need to add a map later, std::map<uint32_t, Slot *> index;
 };
 
 struct Cache {
   std::vector<Set> sets;
 };
+
+// Helper function called either if there's a read-miss or write-miss in write-allocate
+void write_allocate_miss();
--- main.cpp
+++ main.cpp
@@ -7,26 +7,58 @@
 #include <cmath>
 #include <vector>
 #include <string>
+#include <sstream>
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     Cache cache;
-    
-    cache.sets.resize(int(argv[0]));
+
+    // Assign command-line arguments to variables
+    int num_sets = int(argv[0]);
     int blocks_per_set = int(argv[1]);
     int block_size = int(argv[2]);
     bool write_allocate = argv[3]== "write-allocate" ? true : false;  // Okay?
     bool write_back = argv[4]== "write-back" ? true : false;
     std::string eviction_policy = argv[5];
 
-    if(block_size < 4 || ceil(block_size) != floor(block_size) || ceil(blocks_per_set) != floor(blocks_per_set) ||
-      (write_back && !write_allocate)) {
+    // Check if parameters are invalid
+    if(block_size < 4 || ceil(log2(block_size)) != floor(log2(block_size)) || ceil(log2(blocks_per_set)) != floor(log2(blocks_per_set)) ||
+      ceil(log2(num_sets)) != floor(log2(num_sets)) || (write_back && !write_allocate)) {
 
         std::cerr << "Invalid configuration\n";
         return 1;
     }
 
+    cache.sets.resize(num_sets);
 
+    // Determine if cache is direct-mapped
+    bool direct_mapped = false;
+    if (num_sets == 1) {
+      direct_mapped = true;
+    }
+
+    // Setting up variables for results:
+    int total_loads = 0;
+    int total_stores = 0;
+    int load_hits = 0;
+    int load_misses = 0;
+    int store_hits = 0;
+    int store_misses = 0;
+    int total_cycles = 0;
+
+    std::string curr_trace_line;
+
+    // For each line in the input trace file
+    while (std::getline(std::cin, curr_trace_line)) {
+        // Stringstream for current part of line
+        std::istringstream ss(curr_trace_line);
+
+        std::string load_or_store;
+        std::string memory_address;
+        std::string ignored;
+        ss >> load_or_store >> memory_address >> ignored;
+
+        
+    }
 }
 
 #endif
