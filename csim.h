c37f102da16e35b36adcabd65e81904f48aef94e
LeahHersh <lhersh411@gmail.com>
2024-03-12 21:24:17 -0400

    .

--- csim.h
+++ csim.h
@@ -4,7 +4,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdio.h>
-#include <vector>
 
 
 struct Slot {
--- main.cpp
+++ main.cpp
@@ -4,7 +4,6 @@
 #include <stdio.h>
 #include <iostream>
 #include <math.h>
-#include <vector>
 #include <string>
 #include <sstream>
 
