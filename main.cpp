6788a1f9e129a9133f471cd8ee88b39cd0681267
LeahHersh <lhersh411@gmail.com>
2024-03-14 15:20:41 -0400

    .

--- main.cpp
+++ main.cpp
@@ -17,23 +17,23 @@ Slot* find_curr_slot(Cache* cache, uint32_t index, int32_t tag) {
 
   // Find slot with a matching tag or oldest access date
   for (int i = 0; i < set.slots.size(); i++) {
-    Slot curr = set.slots[i];
+    Slot* curr = &(set.slots[i]);
 
-    if (curr.tag == tag) {
-      return &curr;
+    if ((*curr).tag == tag) {
+      return curr;
     }
 
     // Keep track of which slot has the oldest access date
-    if (curr.access_ts < oldest_access) { 
-      oldest_use = &curr;
-      oldest_access = curr.access_ts;
+    if ((*curr).access_ts < oldest_access) { 
+      oldest_use = curr;
+      oldest_access = (*curr).access_ts;
     }
   }
 
   // Evict block used least recently
   (*oldest_use).tag = tag;
   (*oldest_use).valid = true;
-  
+
   return oldest_use;
 }
 
